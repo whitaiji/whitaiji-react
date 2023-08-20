d2b9fcf56cee6560a2423f2ff711a2c058ac2be6
whitaiji <123392795+whitaiji@users.noreply.github.com>
2023-08-20 11:45:24 +0200

    testing

--- src/ExampleComponent.jsx
+++ src/ExampleComponent.jsx
@@ -1,6 +1,8 @@
 import styles from './styles.module.css'
 import React from 'react'
 
-export const ExampleComponent = ({ text }) => {
+ const ExampleComponent = ({ text }) => {
     return <div className={styles.test}>Example Component: {text}</div>
   }
+
+  export default ExampleComponent
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-/* export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-} */
-export { default as ExampleComponent } from './ExampleComponent';
-export { default as VisDiv } from './VisDiv';
-export { default as Loading } from './Loading';
+export const ExampleComponent = ({ text }) => {
+  return <div className={styles.test}>Example Components: {text}</div>
+} 
+
+//export { default as VisDiv } from './VisDiv';
+//export { default as Loading } from './Loading';
